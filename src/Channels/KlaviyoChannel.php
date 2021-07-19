bf4833a7f050cb06c0db1644e1108f2457eee9ee
christophej <christophej@scufgaming.com>
2021-07-19 15:39:45 +0200

    Add back TrackMessage classes

--- src/Channels/KlaviyoChannel.php
+++ src/Channels/KlaviyoChannel.php
@@ -3,6 +3,8 @@
     namespace BrokenTitan\Klaviyo\Channels;
 
     use Illuminate\Notifications\Notification as Notification;
+    use BrokenTitan\Klaviyo\Messages\KlaviyoTrackMessage;
+    use BrokenTitan\Klaviyo\Messages\KlaviyoIdentifyMessage;
     use Klaviyo\Klaviyo;
     use Klaviyo\Model\EventModel as KlaviyoEvent;
     use Klaviyo\Model\ProfileModel as KlaviyoProfile;
@@ -28,24 +30,54 @@
          * @return void
          */
         public function send($notifiable, Notification $notification) {
-            $model = $notification->toKlaviyo($notifiable);
+            $message = $notification->toKlaviyo($notifiable);
+
+            /** Events sent within the same second may have the same default ID so we use the notification ID here. **/
+            if (empty($message->properties['$event_id'])) {
+                $message->properties['$event_id'] = $notification->id;
+            }
             
             switch(true) {
                 default:
-                    throw new Exception("Klaviyo model type does not match a valid type of call.");
+                    throw new Exception("Klaviyo message type does not match a valid type of call.");
                     break;
 
-                case $model instanceof KlaviyoEvent:
-                    $result = $this->klaviyo->publicAPI->track($model);
+                case $message instanceof KlaviyoTrackMessage:
+                    $result = $this->track($message);
                     break;
-
-                case $model instanceof KlaviyoProfile:
-                    $result = $this->klaviyo->publicAPI->identify($model);
+                case $message instanceof KlaviyoIdentifyMessage:
+                    $result = $this->identify($message);
                     break;
             }
 
-            if ($result !== "1") {
+            if ($result !== true) {
                 throw new Exception("Klaviyo event sending returned a failure.");
             }
+        }
+
+        /**
+         * @method track
+         * @param BrokenTitan\Klaviyo\Messages\KlaviyoTrackMessage
+         * @return bool
+         */
+        private function track(KlaviyoTrackMessage $message) : bool {
+            $event = new KlaviyoEvent([
+                'event' => $message->event,
+                'customer_properties' => $message->customer_properties, 
+                'properties' => $message->properties,
+                'time' => $message->time
+            ]);
+
+            return $this->klaviyo->publicAPI->track($event);
+        }
+
+        /**
+         * @method identify
+         * @param BrokenTitan\Klaviyo\Messages\KlaviyoIdentifyMessage
+         * @return bool
+         */
+        private function identify(KlaviyoIdentifyMessage $message) : bool {
+            $profile = new KlaviyoProfile($message->properties);
+            return $this->klaviyo->publicAPI->track($profile);
         }
     }
